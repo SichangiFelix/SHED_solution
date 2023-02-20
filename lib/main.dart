4a7b4270bd534cee68f40c0e3a4b523ac8cfba17
Terry <terrybmochire@gmail.com>
2023-02-20 21:28:36 +0300

    Check if user is logged in or not to determine UI to be displayed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:project/src/features/authentication/auth_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const AuthPage()
     );
   }
 }

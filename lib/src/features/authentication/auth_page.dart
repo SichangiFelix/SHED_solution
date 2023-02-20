1e4fc0d52a0a3ab555bc12dbbab97a9794be3ddf
Terry <terrybmochire@gmail.com>
2023-02-20 21:29:58 +0300

    Listens for auth state changes and updates accordingly.

---
+++ lib/src/features/authentication/auth_page.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:project/src/features/authentication/home_page.dart';
+
+import 'login_page.dart';
+
+class AuthPage extends StatelessWidget{
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if(snapshot.hasData){
+            return HomePage();
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
+

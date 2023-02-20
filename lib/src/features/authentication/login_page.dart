fe37fbb27748020addab9f914e81b2965679a222
Terry <terrybmochire@gmail.com>
2023-02-20 22:14:33 +0300

    Add method to sign in using a google account.

--- lib/src/features/authentication/login_page.dart
+++ lib/src/features/authentication/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginPage extends StatefulWidget{
 
@@ -39,14 +40,34 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+
+  // Sign in with Google method
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication googleAuth = await googleUser!.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            signUserIn();
-            }, child: null),
+            // signUserIn();
+            signInWithGoogle();
+            }, child: const Text("Sign In")),
       ),
     );
   }

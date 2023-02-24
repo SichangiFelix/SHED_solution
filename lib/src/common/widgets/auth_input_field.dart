b73acc007d9f683a6cff825ddefd5e2bc1581fda
SichangiFelix <sichangifelix23@gmail.com>
2023-02-24 14:11:04 +0300

    Reusable auth input field

---
+++ lib/src/common/widgets/auth_input_field.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class AuthInputField extends StatelessWidget {
+
+  TextEditingController controller;
+
+  AuthInputField({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: screenWidth/1.15,
+      child: TextFormField(
+        controller: controller,
+        decoration: const InputDecoration(
+            border: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                )
+            ),
+
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  width: 1,
+                )
+            )
+        ),
+      ),
+    );
+  }
+}

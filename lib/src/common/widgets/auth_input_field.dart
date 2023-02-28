f40e4b5b5b4de04ac242e10ebd661ecd7a405156
SichangiFelix <sichangifelix23@gmail.com>
2023-02-28 13:14:56 +0300

    Obsecured text on password input field

--- lib/src/common/widgets/auth_input_field.dart
+++ lib/src/common/widgets/auth_input_field.dart
@@ -3,10 +3,14 @@ import 'package:flutter/material.dart';
 class AuthInputField extends StatelessWidget {
 
   TextEditingController controller;
+  bool? isObsecured = false;
+  String? inputType;
 
   AuthInputField({
     Key? key,
     required this.controller,
+    this.isObsecured,
+    this.inputType,
   }) : super(key: key);
 
   @override
@@ -18,6 +22,7 @@ class AuthInputField extends StatelessWidget {
       width: screenWidth/1.15,
       height: screenHeight/35,
       child: TextFormField(
+        obscureText: isObsecured == null? false:isObsecured!,
         controller: controller,
         decoration: const InputDecoration(
             border: UnderlineInputBorder(

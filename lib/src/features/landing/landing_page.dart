e5f9c19fe58c1d1f4e51c6753b8743cb1be747b5
SichangiFelix <sichangifelix23@gmail.com>
2023-03-02 12:58:10 +0300

    Edited the landing page

--- lib/src/features/landing/landing_page.dart
+++ lib/src/features/landing/landing_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 import '../authentication/screens/login_page.dart';
 import '../authentication/screens/signup_page.dart';
 
@@ -9,49 +8,81 @@ class LandingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
 
+    return Scaffold(
       body: Center(
         child: Stack(
           children: [
-
-            Container(
-
-             decoration: const BoxDecoration(
-               image: DecorationImage(image: NetworkImage('https://wallpapercave.com/dwp1x/wp5534738.jpg',),fit: BoxFit.cover)
-             ),
-            ),
+            // Container(
+            //  decoration: const BoxDecoration(
+            //    image: DecorationImage(image: NetworkImage('https://wallpapercave.com/dwp1x/wp5534738.jpg',),fit: BoxFit.cover)
+            //  ),
+            // ),
             Align(
-              alignment: Alignment.bottomCenter,
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  margin: EdgeInsets.only(bottom: screenHeight / 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        width: screenWidth / 2.5,
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            )),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const LoginPage()));
+                            },
+                            child: const Text(
+                              "Sign In",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            )),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      SizedBox(
+                        width: screenWidth / 2.5,
+                        child: OutlinedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          const SignupPage()));
+                            },
+                            child: const Text("Sign Up",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500),
+                            )),
+                      ),
+                    ],
+                  ),
+                )),
+            Positioned(
+              top: screenHeight/10,
+              left: screenWidth/30,
               child: Container(
-                margin: const EdgeInsets.only(left: 50,right: 50,bottom: 50),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    ElevatedButton(
-                        onPressed:(){
-                          Navigator.push(context, MaterialPageRoute(
-                              builder: (context)=>const LoginPage()));
-                        }, child: const Text("Sign In")),
-                    ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>const SignupPage()));
-
-                        }, child: const Text("Sign Up")),
-                  ],
-                ),
-              )
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-                child:Container(
-                  margin: const EdgeInsets.only(left: 15,right: 15,top: 50),
-                  child:const Text('Educating the Public on \n '
-                      'Sexual Health Information \n'
-                      '& Awareness',style: TextStyle(
-                      color: Colors.white,fontSize: 26,fontWeight: FontWeight.bold),)
-                ),
-
+                  margin: const EdgeInsets.only(left: 15, right: 15, top: 50),
+                  child: const Text(
+                    'Educating the Public on \n'
+                    'Sexual Health Information \n'
+                    '& Awareness',
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 21,
+                        fontWeight: FontWeight.w600),
+                  )),
             )
           ],
         ),

959b84e54a2b93273ac5bf26af067d6a06306731
SichangiFelix <sichangifelix23@gmail.com>
2023-03-02 13:32:28 +0300

    Instantiated controllers for otp

--- lib/src/features/authentication/screens/otp_page.dart
+++ lib/src/features/authentication/screens/otp_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:project/src/common/widgets/auth_input_field.dart';
 import 'package:project/src/common/widgets/long_blue_button.dart';
 
+import '../common/otp_field.dart';
 import '../common/text_style.dart';
 
 class OtpPage extends StatelessWidget {
@@ -9,6 +10,14 @@ class OtpPage extends StatelessWidget {
   TextEditingController controller = TextEditingController();
 
   OtpPage({Key? key}) : super(key: key);
+
+  //Controllers for otp values
+  TextEditingController otp1 = TextEditingController();
+  TextEditingController otp2 = TextEditingController();
+  TextEditingController otp3 = TextEditingController();
+  TextEditingController otp4 = TextEditingController();
+  TextEditingController otp5 = TextEditingController();
+  TextEditingController otp6 = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -35,7 +44,17 @@ class OtpPage extends StatelessWidget {
               ),
             ),
             SizedBox(height: screenHeight/30,),
-
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                OtpField(controller: otp1,),
+                OtpField(controller: otp2),
+                OtpField(controller: otp3),
+                OtpField(controller: otp4),
+                OtpField(controller: otp5),
+                OtpField(controller: otp6),
+              ],
+            ),
             SizedBox(height: screenHeight/20,),
             LongBlueButton(buttonName: "Confirm", press: (){}),
           ],

a6928eb229e1759776f4a3246bfe7d50fb4f1043
SichangiFelix <sichangifelix23@gmail.com>
2023-03-02 13:29:30 +0300

    Created an otp field

---
+++ lib/src/features/authentication/common/otp_field.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class OtpField extends StatelessWidget {
+  const OtpField({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: 40,
+      child: TextFormField(
+        maxLines: 1,
+        textInputAction: TextInputAction.next,
+        keyboardType: TextInputType.number,
+        onChanged: (_) => FocusScope.of(context).nextFocus(),
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter.digitsOnly,
+          LengthLimitingTextInputFormatter(1)
+        ],
+        decoration: InputDecoration(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(4),
+            borderSide: BorderSide(
+              width: 1,
+            )
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(4),
+              borderSide: BorderSide(
+                width: 1,
+              )
+          ),
+        ),
+      ),
+    );
+  }
+}

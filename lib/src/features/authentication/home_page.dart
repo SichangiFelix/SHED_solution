f117e7603f0c58febe1ac903dd7310443a0c1e94
Terry Mochire <terrybmochire@gmail.com>
2023-03-06 18:26:44 +0300

    Test the read articles method to print to stdout.

---
+++ lib/src/features/authentication/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+      ),
+      body: const Center(
+        child: Text("Home Page"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          logout();
+        },
+        backgroundColor: Colors.green,
+        child: const Text("Click"),
+      )
+    );
+  }
+
+  Future<void> logout() async {
+    await FirebaseAuth.instance.signOut();
+  }
+
+}

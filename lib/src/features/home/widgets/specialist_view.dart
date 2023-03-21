c984354a71d331682ad9baf669e72a008e8fb3f5
Terry Mochire <terrybmochire@gmail.com>
2023-03-21 20:40:20 +0300

    Load specialists from the database.

--- lib/src/features/home/widgets/specialist_view.dart
+++ lib/src/features/home/widgets/specialist_view.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:project/src/features/data/services/specialist_services.dart';
 
 import '../screens/specialists_screen.dart';
 
@@ -31,50 +33,64 @@ class _SpecialistViewState extends State<SpecialistView> {
         ),
         Container(
           height: 120,
-          child: ListView.builder(
-              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
-              scrollDirection: Axis.horizontal,
-              itemCount: 7,
-              itemBuilder: (context, index) {
+          child: StreamBuilder<QuerySnapshot>(
+            stream: SpecialistServices().getSpecialists(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData){
+                List specialists = snapshot.data!.docs;
+                return  ListView.builder(
+                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+                    scrollDirection: Axis.horizontal,
+                    itemCount: specialists.length,
+                    itemBuilder: (context, index) {
 
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>SpecialistScreen()));
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      width: width *0.6,
-                      margin: EdgeInsets.only(left: 3,right: 5),
-                      padding:EdgeInsets.all(18),
-                      color: Colors.grey,
+                     return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>SpecialistScreen()));
+                        },
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Container(
+                            width: width *0.6,
+                            margin: EdgeInsets.only(left: 3,right: 5),
+                            padding:EdgeInsets.all(18),
+                            color: Colors.grey,
 
-                      child: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            CircleAvatar(
-                              radius: 30,
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  CircleAvatar(
+                                    radius: 30,
+                                  ),
+                                  Column(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(specialists[index].data()['name']),
+                                      SizedBox(height:5),
+                                      Text(specialists[index].data()['specialty']),
+                                    ],
+                                  ),
+
+
+                                ],
+                              ),
                             ),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text('Dr Felix Sichangi'),
-                                SizedBox(height:5),
-                                Text('Gynaecologist')
-                              ],
-                            ),
-
-
-                          ],
+                          ),
                         ),
-                      ),
-                    ),
-                  ),
+                      );
+                    });
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
-              }),
+              }
+
+            },
+
+          )
         )
       ],
     );

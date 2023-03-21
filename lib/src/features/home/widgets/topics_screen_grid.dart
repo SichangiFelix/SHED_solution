3a52144f49a3ca9000ccc82805c6a2910922e2d0
asoni304 <jeananderson304@gmail.com>
2023-03-21 16:36:00 +0300

    topic screen,content screen added

--- lib/src/features/home/widgets/topics_screen_grid.dart
+++ lib/src/features/home/widgets/topics_screen_grid.dart
@@ -1,16 +1,14 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:project/src/features/home/screens/topics_screen.dart';
+import 'package:project/src/features/home/screens/topic_content_screen.dart';
 
-class TopicView extends StatefulWidget {
-  String title;
-   TopicView({required this.title});
+class TopicsGrid extends StatefulWidget {
+  const TopicsGrid({Key? key}) : super(key: key);
 
   @override
-  State<TopicView> createState() => _TopicViewState();
+  State<TopicsGrid> createState() => _TopicsGridState();
 }
 
-class _TopicViewState extends State<TopicView> {
+class _TopicsGridState extends State<TopicsGrid> {
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -19,7 +17,7 @@ class _TopicViewState extends State<TopicView> {
 
         Container(
           margin: EdgeInsets.only(left: 20,right: 20),
-          height: MediaQuery.of(context).size.height *0.3,
+          height: MediaQuery.of(context).size.height *0.9,
           child: Center(
             child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
@@ -27,12 +25,12 @@ class _TopicViewState extends State<TopicView> {
                     childAspectRatio: 1.8,
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10),
-                itemCount: 4,
+                itemCount: 17,
                 itemBuilder: (BuildContext ctx, index) {
                   return GestureDetector(
                     onTap: (){
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context)=>TopicScreen()));
+                 Navigator.push(context,
+                     MaterialPageRoute(builder: (context)=>TopicContent()));
                     },
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),
@@ -41,7 +39,7 @@ class _TopicViewState extends State<TopicView> {
                         width: 50,
                         color: Colors.grey,
                         child: Center(
-                            child: Text('Health\nTopics')
+                            child: Text('Contraception')
                         ),
 
                       ),

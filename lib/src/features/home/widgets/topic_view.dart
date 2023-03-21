fe785dba1e1ff48958aee9d4a96c5f9de3fffb10
asoni304 <jeananderson304@gmail.com>
2023-03-21 17:01:46 +0300

    added explore button

--- lib/src/features/home/widgets/topic_view.dart
+++ lib/src/features/home/widgets/topic_view.dart
@@ -2,6 +2,8 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:project/src/features/home/screens/topics_screen.dart';
 
+import '../screens/topic_content_screen.dart';
+
 class TopicView extends StatefulWidget {
   String title;
    TopicView({required this.title});
@@ -16,23 +18,46 @@ class _TopicViewState extends State<TopicView> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-
         Container(
-          margin: EdgeInsets.only(left: 20,right: 20),
+          margin: EdgeInsets.only(left: 10,right: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title,
+                style: const TextStyle(
+                    color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              GestureDetector(
+                onTap: (){
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context)=>TopicScreen()));
+                },
+                child: Text(
+                  'Explore More>',
+                  style: const TextStyle(
+                      color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          margin: EdgeInsets.only(left: 40,right: 40),
           height: MediaQuery.of(context).size.height *0.3,
           child: Center(
             child: GridView.builder(
                 gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 1.8,
-                    crossAxisSpacing: 10,
+                    maxCrossAxisExtent: 180,
+                    childAspectRatio: 1.5,
+                    crossAxisSpacing: 35,
                     mainAxisSpacing: 10),
                 itemCount: 4,
                 itemBuilder: (BuildContext ctx, index) {
                   return GestureDetector(
                     onTap: (){
                       Navigator.push(context,
-                          MaterialPageRoute(builder: (context)=>TopicScreen()));
+                          MaterialPageRoute(builder: (context)=>TopicContent()));
                     },
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(20),

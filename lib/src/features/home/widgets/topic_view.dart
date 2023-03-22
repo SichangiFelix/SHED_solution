04f1358f160e13efd2b35b6a46bae83492f32a16
SichangiFelix <sichangifelix23@gmail.com>
2023-03-23 01:25:40 +0300

    Designed Home screen cards

--- lib/src/features/home/widgets/topic_view.dart
+++ lib/src/features/home/widgets/topic_view.dart
@@ -13,6 +13,9 @@ class TopicView extends StatefulWidget {
 class _TopicViewState extends State<TopicView> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -42,26 +45,164 @@ class _TopicViewState extends State<TopicView> {
           margin: EdgeInsets.only(left: 20,right: 20),
           height: MediaQuery.of(context).size.height *0.3,
           child: Center(
-            child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    childAspectRatio: 1.8,
-                    crossAxisSpacing: 10,
+            child: GridView(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth/16),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1.2,
+                    crossAxisSpacing: screenWidth/10,
                     mainAxisSpacing: 10),
-                itemCount: 4,
-                itemBuilder: (BuildContext ctx, index) {
-                  return ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: Container(
-                      height: 50,
-                      width: 50,
-                      color: Colors.grey,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        //color: Color(0xff06B782),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xff13DEA0),
+                            Color(0xff06B782),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve1.png"),
+                      ),
+                    ),Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve 2.png"),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
                       child: Center(
                           child: Text('Sexual\nHealth')
                       ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        //color: Color(0xff06B782),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xffFC67A7),
+                            Color(0xffF6815B),
+                          ],
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                        ),
                       ),
-                  );
-                }),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve1.png"),
+                      ),
+                    ),Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve 2.png"),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                          child: Text('Sexual\nHealth')
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        //color: Color(0xff06B782),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xff441DFC),
+                            Color(0xff4E81EB),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve1.png"),
+                      ),
+                    ),Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve 2.png"),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                          child: Text('Sexual\nHealth')
+                      ),
+                    ),
+                  ],
+                ),
+                Stack(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        //color: Color(0xff06B782),
+                        gradient: LinearGradient(
+                          colors: [
+                            Color(0xffFFD541),
+                            Color(0xffF0B31A),
+                          ],
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve1.png"),
+                      ),
+                    ),Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Image(
+                        image: AssetImage("assets/images/curve 2.png"),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Center(
+                          child: Text('Sexual\nHealth')
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ],

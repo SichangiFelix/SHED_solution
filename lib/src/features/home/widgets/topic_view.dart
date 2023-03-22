f41260eddd710a0472e92a26a388d666137b9d53
SichangiFelix <sichangifelix23@gmail.com>
2023-03-23 00:16:14 +0300

    Updates

--- lib/src/features/home/widgets/topic_view.dart
+++ lib/src/features/home/widgets/topic_view.dart
@@ -1,6 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:project/src/features/home/screens/topics_screen.dart';
+import 'package:project/src/features/topics/screens/topics_screen.dart';
 
 class TopicView extends StatefulWidget {
   String title;
@@ -16,7 +16,28 @@ class _TopicViewState extends State<TopicView> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.title,
+                style: const TextStyle(
+                    color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+              ),InkWell(
+                onTap: (){
+                  Navigator.pushNamed(context, TopicsScreen.screenRoute);
+                },
+                child: Text(
+                  "Explore More",
+                  style: const TextStyle(
+                      color: Colors.black, fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        ),
         Container(
           margin: EdgeInsets.only(left: 20,right: 20),
           height: MediaQuery.of(context).size.height *0.3,
@@ -29,23 +50,16 @@ class _TopicViewState extends State<TopicView> {
                     mainAxisSpacing: 10),
                 itemCount: 4,
                 itemBuilder: (BuildContext ctx, index) {
-                  return GestureDetector(
-                    onTap: (){
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context)=>TopicScreen()));
-                    },
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: 50,
-                        width: 50,
-                        color: Colors.grey,
-                        child: Center(
-                            child: Text('Health\nTopics')
-                        ),
-
+                  return ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      color: Colors.grey,
+                      child: Center(
+                          child: Text('Sexual\nHealth')
                       ),
-                    ),
+                      ),
                   );
                 }),
           ),
